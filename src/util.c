324484bdd5d25668a9d5af7b6b908dbf57b471b0
Robert Coffey <robertcoffey709@gmail.com>
2026-06-12 01:39:40 -0230

    fix missing null and indexing in string_slice

--- src/util.c
+++ src/util.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 char *string_slice(const char *str, size_t i_start, size_t i_end) {
-    char *copy = malloc(i_end - i_start);
+    char *copy = malloc(i_end - i_start + 1);
     if (!copy) { return NULL; }
     for (size_t i = i_start; i < i_end; ++i) {
-        copy[i] = str[i];
+        copy[i - i_start] = str[i];
     }
+    copy[i_end - i_start] = '\0';
     return copy;
 }
