524fa22d73e84dedcf653defb556bb64b167bef5
Robert Coffey <robertcoffey709@gmail.com>
2026-06-12 01:47:26 -0230

    string_slice: replace loop with memcpy

--- src/util.c
+++ src/util.c
@@ -1,13 +1,12 @@
 #include "util.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 char *string_slice(const char *str, size_t i_start, size_t i_end) {
     char *copy = malloc(i_end - i_start + 1);
     if (!copy) { return NULL; }
-    for (size_t i = i_start; i < i_end; ++i) {
-        copy[i - i_start] = str[i];
-    }
+    memcpy(copy, str + i_start, i_end - i_start);
     copy[i_end - i_start] = '\0';
     return copy;
 }
